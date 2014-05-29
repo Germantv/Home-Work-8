5e90beaa3a21fb7b0ba8d42e0d56eab7cd3177b7
tornato7 <tornato7@gmail.com>
2014-05-28 19:09:33 -0700

    tarting multiply method

--- Quantity.java
+++ Quantity.java
@@ -4,19 +4,23 @@ import java.util.*;
 public class Quantity {
 	protected double value;
 	protected Map units;
+	protected List<String> listUnits;
 	
 	
 	public Quantity(){
 		units = new HashMap<String,Integer>();
+		listUnits = new LinkedList<String>();
 		value = 1;
 	}//end of Q
 	
 	public Quantity(double val, List<String> numer, List<String> denom){
 		units = new HashMap<String,Integer>();
+		listUnits = new LinkedList<String>();
 		value = val;
 		for(String s : numer){
 			if(units.get(s) == null){
 				units.put(s,1);
+				listUnits.add(s);
 			}
 			else{
 				int old = (int)units.get(s);
@@ -38,6 +42,38 @@ public class Quantity {
 		
 		
 	}//end Q
+	
+	public Quantity mul(Quantity q){
+		if(q == null)
+			throw new IllegalArgumentException();
+		
+		Quantity toReturn = new Quantity();
+		
+		HashMap<String,Integer> nQ = new HashMap<String,Integer>();
+		HashMap<String,Integer> oQ = q.getMap();
+		List<String> qlist = q.listUnits();
+		
+//		for(String s:qlist){
+//			if(nQ.get(s) == null){
+//				units.put(s,1);
+//				listUnits.add(s);
+//			}
+//			else{
+//				int old = (int)units.get(s);
+//				units.put(s, old+1);
+//			}
+//		}
+		
+		double ndub = q.getValue() * value;
+		
+		return toReturn;
+	}
+	
+	public List<String> listUnits(){
+		return listUnits;
+	}
+	
+	
 	
 	//end of method
 }
